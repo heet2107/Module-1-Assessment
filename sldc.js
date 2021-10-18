c10ad07979b1ecba950db849aac3cf47be0095ee
Heet Barot <heerbarot21@gmail.com>
2021-10-17 21:31:23 -0400

    Addition implementation working

---
+++ sldc.js
@@ -0,0 +1,33 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Design
+// Implementation
+// Maintain
+// Planning
+// Testing & Integration
+
+// 1) Planning
+// 2) Analysis of Requirements
+// 3) Design
+// 4) Implementation
+// 5) Testing
+// 6) Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/
+
+var Planning = "Start your secure software development by mapping out a timeline, requirements, and any preliminary details necessary.";
+var Analysis = "The organization defines objectives, project goals, and the functions and operations of the application.";
+var Design = "Detailed screen layouts, business rules, process diagrams, pseudocode, and other documentation is laid out. Development begins and secure code is written.";
+var Implementation = "Testing and integration bring all the pieces together in an environment that checks for errors, bugs, vulnerabilities, gaps, and interoperability.";
+var Testing = "Evaluating the quality of software with the aim of finding and fixing defects.";
+var Maintenance = "Once your software is developed, maintaining updates, performance evaluations, and making any changes to the initial software are key maintenance procedures.";
