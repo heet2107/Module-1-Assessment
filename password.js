39fe969908d533b69d8d6645dad176b7c20f81e4
Heet Barot <heerbarot21@gmail.com>
2021-10-17 21:31:49 -0400

    Addition implementation working

---
+++ password.js
@@ -0,0 +1,8 @@
+
+var name = prompt("Enter your password: ");
+    //console.log(`${name} ${name.length}`);
+    if (name.length >= 10){
+    console.log("great success")
+    } else {
+    console.log("too short. Try again")
+    } 
